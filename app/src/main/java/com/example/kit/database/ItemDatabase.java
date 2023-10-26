910a007acecc7c06c64dfa23ba695d1db1d49f20
Hunter Pullishy <hpullish@ualberta.ca>
2023-10-26 11:04:22 -0600

    Added more to the ItemDatabase class

--- app/src/main/java/com/example/kit/database/ItemDatabase.java
+++ app/src/main/java/com/example/kit/database/ItemDatabase.java
@@ -1,4 +1,26 @@
 package com.example.kit.database;
 
+import android.nfc.Tag;
+
+import com.example.kit.data.Item;
+import com.google.firebase.firestore.CollectionReference;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+
 public class ItemDatabase {
+    private CollectionReference db;
+
+    public ItemDatabase(CollectionReference db, <Item> itemList) {
+        this.db = db;
+    }
+
+    public void addItem(){
+        // HashMap<String, String> data = new HashMap<>();
+
+    }
+
+
 }
