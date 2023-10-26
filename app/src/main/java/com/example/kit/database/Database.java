a88b7157782544d728ebb373408ae5a27ad7dafa
Hunter Pullishy <hpullish@ualberta.ca>
2023-10-26 10:50:57 -0600

    Adding database skeleton

--- app/src/main/java/com/example/kit/database/Database.java
+++ app/src/main/java/com/example/kit/database/Database.java
@@ -1,9 +1,9 @@
-package com.example.kit;
+package com.example.kit.database;
 
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
-public class DataBaseAccess {
+public abstract class Database {
     private static FirebaseFirestore db;
     static CollectionReference fetchCollection(String collectionName){
         return db.collection(collectionName);
