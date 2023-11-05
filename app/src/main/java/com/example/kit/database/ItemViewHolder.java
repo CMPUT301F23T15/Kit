933514c33e79058d78e571b1f99ff808eb717c9d
Mahmud Jamal <mahmudjamal014@gmail.com>
2023-11-05 14:12:39 -0700

    Bound tag chips

--- app/src/main/java/com/example/kit/database/ItemViewHolder.java
+++ app/src/main/java/com/example/kit/database/ItemViewHolder.java
@@ -1,13 +1,18 @@
 package com.example.kit.database;
 
+import android.view.View;
+
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.kit.R;
 import com.example.kit.data.Item;
 import com.example.kit.databinding.ItemListRowBinding;
+import com.google.android.material.chip.Chip;
+import com.google.android.material.chip.ChipGroup;
 
 import java.text.DateFormat;
-
-import java.util.Date;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ItemViewHolder extends RecyclerView.ViewHolder {
     private final ItemListRowBinding binding;
@@ -25,5 +30,40 @@ public class ItemViewHolder extends RecyclerView.ViewHolder {
         binding.itemNameRow.setText(item.getName());
         binding.itemDateRow.setText(df.format(item.getAcquisitionDate().toDate()));
         binding.itemValueRow.setText(item.getValue());
+
+        ArrayList<String> tags = item.getTags();
+
+        Chip[] chips = {
+                binding.tagChip1,
+                binding.tagChip2,
+                binding.tagChip3,
+                binding.tagChip4,
+                binding.tagChip5,
+                binding.tagChip6,
+                binding.tagChip7,
+                binding.tagChip8,
+                binding.tagChip9,
+                binding.tagChip10,
+                binding.tagChip11,
+                binding.tagChip12,
+                binding.tagChip13,
+                binding.tagChip14,
+                binding.tagChip15,
+                binding.tagChip16,
+                binding.tagChip17,
+                binding.tagChip18,
+                binding.tagChip19,
+                binding.tagChip20
+        };
+
+        // Populate chips with tags
+        for(int i = 0; i < tags.size(); i++) {
+            // Check for more than chips than
+            if(i >= chips.length) {
+                break;
+            }
+            chips[i].setText(tags.get(i));
+            chips[i].setVisibility(View.VISIBLE);
+        }
     }
 }
