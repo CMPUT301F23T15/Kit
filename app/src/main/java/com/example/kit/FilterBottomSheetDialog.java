b2cfd85b7f471ab7bfbc8ffd7f810f0574fcc891
Jesse-Goertzen <jesse.goertzen2@gmail.com>
2023-11-05 10:52:40 -0700

    started to build the bottom sheet for filter dialog, currenlty not complete

---
+++ app/src/main/java/com/example/kit/FilterBottomSheetDialog.java
@@ -0,0 +1,15 @@
+package com.example.kit;
+
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.material.bottomsheet.BottomSheetDialog;
+
+public class FilterBottomSheetDialog extends BottomSheetDialog {
+    public FilterBottomSheetDialog(@NonNull Context context) {
+        super(context);
+    }
+
+
+}
