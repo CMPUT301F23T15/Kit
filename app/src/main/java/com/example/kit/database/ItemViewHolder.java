9120389ccca4c2349cde5a6a9ee83148427a8588
Mahmud Jamal <mahmudjamal014@gmail.com>
2023-11-05 12:39:45 -0700

    displayItem method of ItemViewHolder - need tags

--- app/src/main/java/com/example/kit/database/ItemViewHolder.java
+++ app/src/main/java/com/example/kit/database/ItemViewHolder.java
@@ -5,6 +5,10 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.example.kit.data.Item;
 import com.example.kit.databinding.ItemListRowBinding;
 
+import java.text.DateFormat;
+
+import java.util.Date;
+
 public class ItemViewHolder extends RecyclerView.ViewHolder {
     private final ItemListRowBinding binding;
 
@@ -15,6 +19,11 @@ public class ItemViewHolder extends RecyclerView.ViewHolder {
 
     // TODO: Expand
     public void displayItem(Item item){
+        // Fill in item row values
+
+        DateFormat df = DateFormat.getDateTimeInstance();
         binding.itemNameRow.setText(item.getName());
+        binding.itemDateRow.setText(df.format(item.getAcquisitionDate().toDate()));
+        binding.itemValueRow.setText(item.getValue());
     }
 }
