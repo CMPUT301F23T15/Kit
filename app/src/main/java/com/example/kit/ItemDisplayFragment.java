2c2ff6edb3996923e3710647086c2175ab4dae2f
Jesse-Goertzen <jesse.goertzen2@gmail.com>
2023-11-07 17:57:52 -0700

    made the function to build an item from the values in the display item fragment

--- app/src/main/java/com/example/kit/ItemDisplayFragment.java
+++ app/src/main/java/com/example/kit/ItemDisplayFragment.java
@@ -8,18 +8,16 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.navigation.NavBackStackEntry;
 import androidx.navigation.NavController;
-import androidx.navigation.NavDestination;
-import androidx.navigation.Navigation;
 import androidx.navigation.fragment.NavHostFragment;
 
 import com.example.kit.data.Item;
 import com.example.kit.databinding.ItemDisplayBinding;
+import com.google.android.material.chip.Chip;
 
 public class ItemDisplayFragment extends Fragment {
 
-    private ItemDisplayBinding itemDisplayBinding;
+    private ItemDisplayBinding binding;
     private NavController navController;
     private boolean newItem;
 
@@ -37,10 +35,30 @@ public class ItemDisplayFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        itemDisplayBinding = ItemDisplayBinding.inflate(inflater, container, false);
-        return itemDisplayBinding.getRoot();
+        binding = ItemDisplayBinding.inflate(inflater, container, false);
+        return binding.getRoot();
     }
 
+    private Item buildItem() {
+        Item newItem = new Item();
 
+        newItem.setName(binding.itemNameDisplay.getText().toString());
+        newItem.setValue(binding.itemValueDisplay.getText().toString());
+        newItem.setDescription(binding.itemDescriptionDisplay.getText().toString());
+        newItem.setComment(binding.itemCommentDisplay.getText().toString());
+        newItem.setMake(binding.itemMakeDisplay.getText().toString());
+        newItem.setModel(binding.itemModelDisplay.getText().toString());
+//        newItem.setSerialNumber(itemDisplayBinding.); TODO: Add this to XML, Jesse Forgot
 
+        // Tags are sort of 1 indexed because the first tag is the add new tag button
+        int numTags = binding.itemDisplayTagGroup.getChildCount();
+        for (int i = 1; i < numTags; i++) {
+            Chip chip = (Chip) binding.itemDisplayTagGroup.getChildAt(i);
+            if (!chip.getText().toString().isEmpty()) {
+                newItem.addTag(chip.getText().toString());
+            }
+        }
+
+        return newItem;
+    }
 }
--- app/src/main/java/com/example/kit/data/Item.java
+++ app/src/main/java/com/example/kit/data/Item.java
@@ -131,11 +131,11 @@ public class Item {
         return tags;
     }
 
-//    public void addTag(Tag tag) {
-//        if (!tags.contains(tag)) {
-//            tags.add(tag);
-//        }
-//    }
+    public void addTag(String tag) {
+        if (!tags.contains(tag)) {
+            tags.add(tag);
+        }
+    }
 
     public String getId() {return id; }
 
