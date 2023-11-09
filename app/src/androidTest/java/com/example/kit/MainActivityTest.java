ecb60ab8f3c3ddadc8f56b769eee101ea42decd8
Mahmud Jamal <mahmudjamal014@gmail.com>
2023-11-09 13:40:53 -0700

    Defined tests

--- app/src/androidTest/java/com/example/kit/MainActivityTest.java
+++ app/src/androidTest/java/com/example/kit/MainActivityTest.java
@@ -27,9 +27,38 @@ public class MainActivityTest {
     public ActivityScenarioRule<MainActivity> scenario = new
             ActivityScenarioRule<MainActivity>(MainActivity.class);
 
+    public void testCreateItem() {
+        // Test if an item is properly created. Check field values
+    }
+
+    @Test
+    public void testAddTag() {
+        // Test if a tag gets properly added to an item
+    }
+
+    @Test
+    public void testChangeName() {
+        // Test if changing the 'name' field is properly reflected
+    }
+
+    @Test
+    public void testChangeDate() {
+        // Test if changing the 'name' field is properly reflected
+    }
+
+    @Test
+    public void testChangeValue() {
+        // Test if changing the 'name' field is properly reflected
+    }
+
     @Test
     public void testTotalValuation() {
         // Test if total valuation equals the sum of all values in item list
         // onData(is(instanceOf(String.class))).inAdapterView(withId(R.id.item_list)).atPosition(0).check(matches((withText("$1.00"))));
+    }
+
+    @Test
+    public void testDeleteItem() {
+        // Test if an item is properly deleted
     }
 }
