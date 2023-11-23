61a64c4d9f560bdf763f326106c39b98bdaf9327
HunterPullishy <hunterpullishy@gmail.com>
2023-11-23 08:58:41 -0700

    Adding profile fragment standard methods

--- app/src/main/java/com/example/kit/ProfilePageFragment.java
+++ app/src/main/java/com/example/kit/ProfilePageFragment.java
@@ -1,4 +1,36 @@
 package com.example.kit;
 
-public class ProfilePageFragment {
+import android.os.Bundle;
+import android.renderscript.ScriptGroup;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+
+import com.example.kit.databinding.ItemListBinding;
+import com.example.kit.databinding.ProfilePageBinding;
+
+public class ProfilePageFragment extends Fragment {
+    private ProfilePageBinding binding;
+
+    private NavController navController;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        navController = NavHostFragment.findNavController(this);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        binding = ProfilePageBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
 }
