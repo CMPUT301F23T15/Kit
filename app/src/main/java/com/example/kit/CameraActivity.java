5b9678a046376d0f7e1975ab55014a59e409d2ae
aryankumar2811 <askumar1@ualberta.ca>
2023-11-29 22:11:58 -0700

    Update CameraActivity.java

--- app/src/main/java/com/example/kit/CameraActivity.java
+++ app/src/main/java/com/example/kit/CameraActivity.java
@@ -1,22 +1,33 @@
 package com.example.kit;
 
+import android.content.ContentValues;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.camera.core.CameraSelector;
+import androidx.camera.core.ImageCapture;
+import androidx.camera.core.ImageCaptureException;
 import androidx.camera.core.Preview;
 import androidx.camera.lifecycle.ProcessCameraProvider;
 import androidx.core.content.ContextCompat;
 import com.example.kit.databinding.CameraBinding; // replace with your actual binding class
 import com.google.common.util.concurrent.ListenableFuture;
 
+import java.util.Locale;
 import java.util.concurrent.ExecutionException;
+import java.text.SimpleDateFormat;
 
 public class CameraActivity extends AppCompatActivity {
 
     private static final String TAG = "CameraActivity";
     private CameraBinding viewBinding; // replace with your actual binding class
+    private ImageCapture imageCapture;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -43,11 +54,21 @@ public class CameraActivity extends AppCompatActivity {
                 Preview preview = new Preview.Builder().build();
                 preview.setSurfaceProvider(viewBinding.viewFinder.getSurfaceProvider());
 
+                imageCapture = new ImageCapture.Builder().build();
                 // Select back camera as a default
                 CameraSelector cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA;
-                cameraProvider.unbindAll();
-                cameraProvider.bindToLifecycle(
-                        this, cameraSelector, preview);
+
+                try {
+                    // Unbind use cases before rebinding
+                    cameraProvider.unbindAll();
+
+                    // Bind use cases to camera
+                    cameraProvider.bindToLifecycle(
+                            this, cameraSelector, preview, imageCapture);
+
+                } catch (Exception exc) {
+                    Log.e(TAG, "Use case binding failed", exc);
+                }
 
             } catch (ExecutionException | InterruptedException e) {
                 Log.e(TAG, "Error starting camera", e);
@@ -55,4 +76,48 @@ public class CameraActivity extends AppCompatActivity {
 
         }, ContextCompat.getMainExecutor(this));
     }
+    private void takePhoto() {
+        Log.d("Capture", "reached");
+        // Get a stable reference of the modifiable image capture use case
+        ImageCapture imageCapture = this.imageCapture;
+        if (imageCapture == null) return;
+        final String FILENAME_FORMAT = "yyyyMMdd_HHmmssSSS";
+
+        // Create time stamped name and MediaStore entry.
+        String name = new SimpleDateFormat(FILENAME_FORMAT, Locale.US).format(System.currentTimeMillis());
+
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(MediaStore.MediaColumns.DISPLAY_NAME, name);
+        contentValues.put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg");
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.P) {
+            contentValues.put(MediaStore.Images.Media.RELATIVE_PATH, "Pictures/CameraX-Image");
+        }
+
+        // Create output options object which contains file + metadata
+        ImageCapture.OutputFileOptions outputOptions = new ImageCapture.OutputFileOptions.Builder(
+                getContentResolver(),
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                contentValues)
+                .build();
+
+        // Set up image capture listener, which is triggered after the photo has been taken
+        imageCapture.takePicture(
+                outputOptions,
+                ContextCompat.getMainExecutor(this),
+                new ImageCapture.OnImageSavedCallback() {
+                    @Override
+                    public void onError( @NonNull ImageCaptureException exc) {
+                        Log.e(TAG, "Photo capture failed: " + exc.getMessage(), exc);
+                    }
+
+                    @Override
+                    public void onImageSaved(@NonNull ImageCapture.OutputFileResults output) {
+                        String msg = "Photo capture succeeded: " + output.getSavedUri();
+                        Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
+                        Log.d(TAG, msg);
+                    }
+                }
+        );
+    }
+
 }
